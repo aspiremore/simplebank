7cc189196262c7942187dfaa86497529ccce08bc
Aiswarya <avinayac@cisco.com>
2023-05-29 16:46:32 +0530

    added viper to handle configs

--- db/tests/main_test.go
+++ db/tests/main_test.go
@@ -2,23 +2,25 @@ package tests
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
 	"github/aspiremore/simplebank/db/sqlc"
+	"github/aspiremore/simplebank/db/util"
 	"log"
 	"os"
 	"testing"
+
+	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
 var testDB *sql.DB
 var testQueries *db.Queries
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("unable to load configurations", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("We cannot connect to the DB:", err)
 	}
--- main.go
+++ main.go
@@ -4,25 +4,25 @@ import (
 	"database/sql"
 	"github/aspiremore/simplebank/api"
 	db "github/aspiremore/simplebank/db/sqlc"
+	"github/aspiremore/simplebank/db/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	local_address = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("unable to load configurations", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("We cannot connect to the DB:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err  = server.Start(local_address)
+	err  = server.Start(config.Server_address)
 	if err != nil {
 		log.Fatal("cannot start server ", err)
 	}
